04340bc0f346173e5a951088a92a4cf3e87af187
Rinat Silnov <mail@rinat.io>
2013-05-23 04:18:09 +0400

    Updated composer. Added Yii autoloader in index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 if (!empty($_SERVER['YII_DEBUG']) || getenv('YII_DEBUG')) {
     defined('YII_DEBUG') or define('YII_DEBUG',true);
 }
-require '../vendor/autoload.php';
+//Uncomment following line if you wish to use Yii autoloader first
+//require __DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = new yii\web\Application(require('../config/main.php'));
 $app->run();
